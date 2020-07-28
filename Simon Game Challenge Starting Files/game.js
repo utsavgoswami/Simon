f815adec0574c72b9cd1731e5b4c399cb5dc823f
Utsav Goswami <utsavg19@googlemail.com>
2020-07-27 23:12:17 -0700

    Gameplay flow algorithm added

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,11 @@
 // The pattern that the user needs to match in order to 
 // "win" the level
+
+let gameStarted = false;
+let level = 0;
+
 const gamePattern = [];
+const userPattern = [];
 
 const sounds = {
     'red': new Audio('sounds/red.mp3'),
@@ -14,6 +19,10 @@ const buttonColors = ["red", "blue", "green", "yellow"];
 
 // Randomly generates the next move that the user must perform
 function nextSequence() {
+    ++level;
+
+    $("h1").text("Level " + level);
+
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColor = buttonColors[randomNumber];
 
@@ -30,6 +39,29 @@ function nextSequence() {
 
 $(".btn").on("click", function() {
     const userChosenColor = this.id;
+
+    userPattern.push(userChosenColor);
+
+    // Check whether userPattern matches game pattern
+    if (gamePattern.length === userPattern.length) {
+        let totalMatches = 0;
+        for (let i = 0; i < gamePattern.length; i++) {
+            if (gamePattern[i] === userPattern[i]) {
+                ++totalMatches;
+            }
+        }
+
+        if (totalMatches === gamePattern.length) {
+            // Reset user pattern history
+            userPattern.length = 0;
+
+            setTimeout(function() {
+                // Move on to next round 
+                nextSequence();
+            }, 600);
+
+        }
+    }
 
     // Play sound
     playSound(userChosenColor);
@@ -51,3 +83,14 @@ function animatePress(currentColor) {
         btn.removeClass("pressed");
     }, 100);
 }
+
+$(document).keydown(function (event) {
+
+    // For starting the game 
+    if (!gameStarted) {
+        gameStarted = true;
+        nextSequence();
+
+        $("h1").text("Level " + level);
+    }
+})
